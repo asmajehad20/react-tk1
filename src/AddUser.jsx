f3bf9e51f8e570324ecef737f2e6eea4dcca0f69
asmajehad20 <asmajehad919@gmail.com>
2025-05-04 00:44:30 +0300

    first commit

--- src/AddUser.jsx
+++ src/AddUser.jsx
@@ -1,19 +1,59 @@
-import CustomInput from './shared/CustomInput';
-import CustomButton from './shared/CustomButton';
+// import CustomInput from './shared/CustomInput';
+// import CustomButton from './shared/CustomButton';
 import './AddUser.css';
 
-function AddUser() {
-  return (
-    <form className="form">
-      <CustomInput label="Name" placeholder="Enter name" />
-      <CustomInput label="ID" placeholder="Enter ID" />
-      <CustomInput label="Email" placeholder="Enter email" />
-      <CustomInput label="Status" placeholder="Enter status" />
-      <CustomInput label="Age" placeholder="Enter age" />
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-      <CustomButton text="Submit" anotherClass="submit-btn" />
-    </form>
-  );
+export default function AddUser(){
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getProducts = async()=>{
+    try{
+      const{data} = await axios.get("https://dummyjson.com/products");
+      setProducts(data.products);
+    }catch(err){
+      setError(err.message);
+    }finally{
+      setIsLoading(false);
+    }
+  }
+  useEffect(function(){
+    getProducts();
+  }, []);
+
+  if(isLoading){
+    return <div className='alert alert-success'>wait</div>
+  }
+  if(error){
+    return <div className='text text-danger'>{error}</div>
+  }
+  return(
+    <div className='container'>
+    { products.map(product=>
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <img src = {product.thumbnail} />
+        </div>
+    )}
+    </div>
+  )
 }
 
-export default AddUser;
+// function AddUser() {
+//   return (
+//     <form className="form">
+//       <CustomInput label="Name" placeholder="Enter name" />
+//       <CustomInput label="ID" placeholder="Enter ID" />
+//       <CustomInput label="Email" placeholder="Enter email" />
+//       <CustomInput label="Status" placeholder="Enter status" />
+//       <CustomInput label="Age" placeholder="Enter age" />
+
+//       <CustomButton text="Submit" anotherClass="submit-btn" />
+//     </form>
+//   );
+// }
+
+// export default AddUser;
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
 import {useState} from "react";
+import {useRef} from "react";
 import User from "./User.jsx";
 import AddUser from "./AddUser.jsx";
 function App(){
-  const [users, setUsers] = useState([
-    {"id":1, "name":"tariq", "email":"tariq@gmail.com", "isBlocked":true},
-    {"id":2, "name":"anas", "email":"anas@gmail.com", "isBlocked":false},
-    {"id":3, "name":"hala", "email":"hala@gmail.com", "isBlocked":true},
-    {"id":4, "name":"asmaa", "email":"asmaa@gmail.com", "isBlocked":true},
-  ]);
+  // const [users, setUsers] = useState([
+  //   {"id":1, "name":"tariq", "email":"tariq@gmail.com", "isBlocked":true},
+  //   {"id":2, "name":"anas", "email":"anas@gmail.com", "isBlocked":false},
+  //   {"id":3, "name":"hala", "email":"hala@gmail.com", "isBlocked":true},
+  //   {"id":4, "name":"asmaa", "email":"asmaa@gmail.com", "isBlocked":true},
+  // ]);
+
+  const nameInput = useRef();
+  const registerUser = (e)=>{
+    e.preventDefault();
+    console.log(nameInput.current.value);
+  }
+  const handleChange = ()=>{
+    console.log(nameInput.current.value);
+  }
 
   return(
     <div>
+      <h2>Register</h2>
+      <form onSubmit={registerUser}>
+        <div className="mb-3">
+          <label className="form-label">user name</label>
+          <input ref={nameInput}onChange={handleChange} type="text" className="form-control" />
+
+        </div>
+        <button type="submit" className="btn btn-outline-primary">Register</button>
+      </form>
       {/* <div className="users">
       {
         users.map((user)=>
           <User name={user.name} email={user.email} key="user.id"/>
         )
       }
-      </div> */}
-      <AddUser />
+      </div>
+      <AddUser /> */}
     </div>
     
   );
